97bed5d7148b40b7c37eb6bc3ee6c3eff9444d08
haminhsang1903 <adventurer15999@gmail.com>
2020-10-04 17:19:24 +0700

    fix cors

--- src/main/java/com/example/demoJWT/AddCorsMapping.java
+++ src/main/java/com/example/demoJWT/AddCorsMapping.java
@@ -1,8 +1,9 @@
 package com.example.demoJWT;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class AddCorsMapping implements WebMvcConfigurer{
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
